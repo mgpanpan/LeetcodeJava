c411d525bf15279dfdba369f1ba89f4997777ef5
mgpanpan <pmengguan@gmail.com>
2015-11-20 12:56:09 +0800

    Simplify

--- src/RemoveDuplicatesSortedListII.java
+++ src/RemoveDuplicatesSortedListII.java
@@ -2,7 +2,6 @@
  * Created by pmg on 2015/11/18.
  * Given a sorted linked list, delete all nodes that have duplicate numbers, leaving only distinct numbers from the original list.
  *
- * Have you met this question in a real interview? Yes
  * Example
  * Given 1->2->3->3->4->4->5, return 1->2->5.
  * Given 1->1->1->2->3, return 2->3.
